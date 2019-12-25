e5ba35e401bfe926179f14d8d065a2de1a159014
Ahmed Ayman <ahmedayman055g@gmail.com>
2019-12-25 20:09:47 +0200

    remove gatsby files

--- styles-66bdc183d027a71309cd.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"+pfR":function(n,o,p){},"0sYV":function(n,o,p){}}]);
-//# sourceMappingURL=styles-66bdc183d027a71309cd.js.map
--- styles-831ffdcf08c6a5663d5c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"0sYV":function(n,w,o){}}]);
-//# sourceMappingURL=styles-831ffdcf08c6a5663d5c.js.map
